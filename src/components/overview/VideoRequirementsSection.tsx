a6de2a016d5c552cff65bb47b7eecf05383c3338
japanese-knife <japanese.traditional.knife@gmail.com>
2026-01-04 14:13:54 +0900

    Updated VideoRequirementsSection.tsx

--- src/components/overview/VideoRequirementsSection.tsx
+++ src/components/overview/VideoRequirementsSection.tsx
@@ -31,7 +31,7 @@ export default function VideoRequirementsSection({ projectId, readOnly = false }
       .from('video_requirements')
       .select('*')
       .eq('project_id', projectId)
-      .order('created_at', { ascending: false });
+      .order('created_at', { ascending: true });
 
     setRequirements(data || []);
   };
