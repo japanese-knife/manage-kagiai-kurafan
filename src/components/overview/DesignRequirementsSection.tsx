4547d13e5c75849627e5d06c7d5b7e19b865f601
japanese-knife <japanese.traditional.knife@gmail.com>
2026-01-04 14:02:58 +0900

    Updated DesignRequirementsSection.tsx

--- src/components/overview/DesignRequirementsSection.tsx
+++ src/components/overview/DesignRequirementsSection.tsx
@@ -208,7 +208,6 @@ export default function DesignRequirementsSection({ projectId, readOnly = false 
               </div>
             ))}
           </div>
-        </>
       )}
     </section>
   );
