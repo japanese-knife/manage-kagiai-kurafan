08a4a1a78d40b61548f2789f8247fc2ddaf7e0ea
japanese-knife <japanese.traditional.knife@gmail.com>
2026-01-12 16:35:58 +0900

    Updated ProjectScheduleView.tsx

--- src/components/ProjectScheduleView.tsx
+++ src/components/ProjectScheduleView.tsx
@@ -726,10 +726,10 @@ export default function ProjectScheduleView({ user, activeBrandTab, viewType, on
   
   return (
     <tr>
-      <th className="sticky left-0 z-30 bg-neutral-50 border border-neutral-200 px-4 py-2 text-left font-semibold text-neutral-900 min-w-[200px]">
-        {headerLabel}
-      </th>
-      <th className="sticky left-[200px] z-30 bg-neutral-50 border border-neutral-200 px-2 py-2 text-center font-semibold text-neutral-900 w-[60px]">
+      <th className="sticky left-0 z-40 bg-neutral-50 border border-neutral-200 px-4 py-2 text-left font-semibold text-neutral-900 min-w-[200px]">
+  {headerLabel}
+</th>
+<th className="sticky left-[200px] z-40 bg-neutral-50 border border-neutral-200 px-2 py-2 text-center font-semibold text-neutral-900 w-[60px]">
   リンク
 </th>
         {dates.map((date, index) => (
