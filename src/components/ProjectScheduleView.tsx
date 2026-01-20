353ab450a7d8b1801d5f4414a2a8e3726a8e3363
japanese-knife <japanese.traditional.knife@gmail.com>
2026-01-20 13:34:16 +0900

    Updated ProjectScheduleView.tsx

--- src/components/ProjectScheduleView.tsx
+++ src/components/ProjectScheduleView.tsx
@@ -589,7 +589,9 @@ const isCurrentMonth = (date: Date): boolean => {
       case 'ArrowLeft':
         e.preventDefault();
         if (currentDateIndex > 0) {
-          const newDate = dates[currentDateIndex - 1].toISOString().split('T')[0];
+          const newDate = viewType === 'monthly'
+            ? `${dates[currentDateIndex - 1].getFullYear()}-${String(dates[currentDateIndex - 1].getMonth() + 1).padStart(2, '0')}`
+            : dates[currentDateIndex - 1].toISOString().split('T')[0];
           setSelectedCell({ 
             projectId: selectedCell.projectId, 
             date: newDate
